a5169a8aa2a3b8cca83cf7016049a328a8ec789d
Johannes Günther <johannes.guenther@gmx.eu>
2013-04-17 08:17:14 +0200

    move Manfred's "Das ist das" to Misc and Jothe's "Das ist das" to Klassiker

--- src/main/java/panelfx/PanelView.java
+++ src/main/java/panelfx/PanelView.java
@@ -3,8 +3,6 @@ package panelfx;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -22,195 +20,201 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.annotation.PostConstruct;
+
 public class PanelView {
 
-    private Scene scene;
-    private final Map<Sound, Button> soundButtons = new HashMap<>();
-    private ImageView muteView;
-    private TabPane tabPane;
-    private Tab primaryTab;
-    private Tab movieTab;
-    private Tab newbiesTab;
+	private Scene scene;
+	private final Map<Sound, Button> soundButtons = new HashMap<>();
+	private ImageView muteView;
+	private TabPane tabPane;
+	private Tab primaryTab;
+	private Tab movieTab;
+	private Tab newbiesTab;
 
-    public void registerSoundButtonActionListener(final EventHandler<ActionEvent> pushButtonEvent) {
+	public void registerSoundButtonActionListener(final EventHandler<ActionEvent> pushButtonEvent) {
 
-        for (final Button button : this.soundButtons.values()) {
-            button.setOnAction(pushButtonEvent);
-        }
-    }
+		for (final Button button : this.soundButtons.values()) {
+			button.setOnAction(pushButtonEvent);
+		}
+	}
 
-    public void registerControlActionListener(final EventHandler<MouseEvent> controlEvent) {
-        this.muteView.setOnMouseClicked(controlEvent);
-    }
+	public void registerControlActionListener(final EventHandler<MouseEvent> controlEvent) {
+		this.muteView.setOnMouseClicked(controlEvent);
+	}
 
-    public void show(final Stage stage) {
-        stage.getIcons().add(new Image(PanelFX.class.getResourceAsStream("icon.png")));
+	public void show(final Stage stage) {
+		stage.getIcons().add(new Image(PanelFX.class.getResourceAsStream("icon.png")));
 
-        stage.setTitle("The Panel FX");
-        stage.setScene(this.scene);
-        stage.setResizable(false);
-        stage.show();
-    }
+		stage.setTitle("The Panel FX");
+		stage.setScene(this.scene);
+		stage.setResizable(false);
+		stage.show();
+	}
 
-    private void createButtons() {
+	private void createButtons() {
 
-        for (final Sound sound : Sound.values()) {
-            final Button button = new Button(sound.getLabel());
-            button.setId(sound.getFilename());
-            button.setMinWidth(110);
+		for (final Sound sound : Sound.values()) {
+			final Button button = new Button(sound.getLabel());
+			button.setId(sound.getFilename());
+			button.setMinWidth(110);
 
-            this.soundButtons.put(sound, button);
-        }
-    }
+			this.soundButtons.put(sound, button);
+		}
+	}
 
-    @PostConstruct
-    public void build() {
+	@PostConstruct
+	public void build() {
 
-        this.createButtons();
+		createButtons();
 
-        final FlowPane masterPane = new FlowPane();
-        masterPane.setPadding(new Insets(5, 20, 30, 20));
-        masterPane.setVgap(5);
-        masterPane.setHgap(5);
+		final FlowPane masterPane = new FlowPane();
+		masterPane.setPadding(new Insets(5, 20, 30, 20));
+		masterPane.setVgap(5);
+		masterPane.setHgap(5);
 
-        final FlowPane controllingPane = this.createFlowPane(false);
-        final Image muteImage = new Image(this.getClass().getResourceAsStream("mute.png"));
-        this.muteView = new ImageView(muteImage);
-        this.muteView.setId("STOP");
-        this.muteView.setCursor(Cursor.HAND);
-        controllingPane.getChildren().add(this.muteView);
-        controllingPane.setAlignment(Pos.CENTER_RIGHT);
-        masterPane.getChildren().add(controllingPane);
+		final FlowPane controllingPane = this.createFlowPane(false);
+		final Image muteImage = new Image(this.getClass().getResourceAsStream("mute.png"));
+		this.muteView = new ImageView(muteImage);
+		this.muteView.setId("STOP");
+		this.muteView.setCursor(Cursor.HAND);
+		controllingPane.getChildren().add(this.muteView);
+		controllingPane.setAlignment(Pos.CENTER_RIGHT);
+		masterPane.getChildren().add(controllingPane);
 
-        this.tabPane = new TabPane();
+		this.tabPane = new TabPane();
 
-        masterPane.getChildren().add(this.tabPane);
+		masterPane.getChildren().add(this.tabPane);
 
-        this.createPrimaryTab();
-        this.createMovieTab();
-        this.createClassicsTab();
+		createPrimaryTab();
+		createMovieTab();
+		createClassicsTab();
 
-        this.scene = new Scene(masterPane);
-        this.scene.getStylesheets().addAll(PanelFX.class.getResource("stylesheet.css").toExternalForm());
-    }
+		this.scene = new Scene(masterPane);
+		this.scene.getStylesheets().addAll(PanelFX.class.getResource("stylesheet.css").toExternalForm());
+	}
 
-    private void createPrimaryTab() {
+	private void createPrimaryTab() {
 
-        this.primaryTab = new Tab("Hits");
-        this.primaryTab.setClosable(false);
-        this.tabPane.getTabs().add(this.primaryTab);
+		this.primaryTab = new Tab("Hits");
+		this.primaryTab.setClosable(false);
+		this.tabPane.getTabs().add(this.primaryTab);
 
-        final FlowPane primaryTabContent = new FlowPane();
-        primaryTabContent.setPadding(new Insets(10, 0, 0, 0));
-        primaryTabContent.getStyleClass().add("wood-shifted");
-        this.primaryTab.setContent(primaryTabContent);
+		final FlowPane primaryTabContent = new FlowPane();
+		primaryTabContent.setPadding(new Insets(10, 0, 0, 0));
+		primaryTabContent.getStyleClass().add("wood-shifted");
+		this.primaryTab.setContent(primaryTabContent);
 
-        final FlowPane paneBastelecke = this.createFlowPane(Sound.STANDUP, Sound.CUBAGE, Sound.BATTLE1, Sound.BATTLE3, Sound.DOORBELL, Sound.ALARM,
-                Sound.PUSH_IT, Sound.OEOEOEEY, Sound.LAUSCHANGRIFF, Sound.FOG, Sound.BAEM, Sound.ERBAERMLICH, Sound.HORHOR, Sound.NEIN_ULTZ, Sound.HILFE,
-                Sound.COOL, Sound.KNARZ, Sound.SORRY);
+		final FlowPane paneBastelecke = this.createFlowPane(Sound.STANDUP, Sound.CUBAGE, Sound.BATTLE1, Sound.BATTLE3, Sound.DOORBELL, Sound.ALARM,
+				Sound.PUSH_IT, Sound.OEOEOEEY, Sound.LAUSCHANGRIFF, Sound.FOG, Sound.BAEM, Sound.ERBAERMLICH, Sound.HORHOR, Sound.NEIN_ULTZ, Sound.HILFE,
+				Sound.COOL, Sound.KNARZ, Sound.SORRY);
 
-        primaryTabContent.getChildren().add(this.createLabel("Bastelecke"));
-        primaryTabContent.getChildren().add(paneBastelecke);
+		primaryTabContent.getChildren().add(createLabel("Bastelecke"));
+		primaryTabContent.getChildren().add(paneBastelecke);
 
-        final FlowPane paneFrank = this.createFlowPane(Sound.NEIN_FRALLER, Sound.AAAAAAH, Sound.FALSCH, Sound.DANKOEOE, Sound.BULLSHIT, Sound.KOTZEN,
-                Sound.HUST, Sound.FRANK_MUELLER, Sound.ZUR_LIEFERUNG, Sound.SCHAFSKAESE, Sound.STEELE2);
+		final FlowPane paneFrank = this.createFlowPane(Sound.NEIN_FRALLER, Sound.AAAAAAH, Sound.FALSCH, Sound.DANKOEOE, Sound.BULLSHIT, Sound.KOTZEN,
+				Sound.HUST, Sound.FRANK_MUELLER, Sound.ZUR_LIEFERUNG, Sound.SCHAFSKAESE, Sound.STEELE2);
 
-        primaryTabContent.getChildren().add(this.createLabel("Frank"));
-        primaryTabContent.getChildren().add(paneFrank);
+		primaryTabContent.getChildren().add(createLabel("Frank"));
+		primaryTabContent.getChildren().add(paneFrank);
 
-        final FlowPane paneMisc = this.createFlowPane(Sound.BIEKER, Sound.BALLAD, Sound.JEOPARDY, Sound.REMOTE, Sound.DELETE, Sound.SNAPHOT, Sound.KHAN,
-                Sound.ASSERT, Sound.WIE_GEIL, Sound.FAIL, Sound.FAIL_SATANIC, Sound.LAUGHING_DIYER, Sound.GANZ_KLAR, Sound.ALTOBELLI, Sound.TACH, Sound.LOKAL,
-                Sound.WAS_DENN, Sound.NJAEH);
+		final FlowPane paneMisc = this.createFlowPane(Sound.WAS_DU_GLAUBST_MAROHDE, Sound.JEOPARDY, Sound.REMOTE, Sound.DELETE, Sound.SNAPHOT, Sound.KHAN,
+				Sound.ASSERT, Sound.WIE_GEIL, Sound.FAIL, Sound.FAIL_SATANIC, Sound.LAUGHING_DIYER, Sound.GANZ_KLAR, Sound.ALTOBELLI, Sound.TACH, Sound.LOKAL,
+				Sound.WAS_DENN, Sound.NJAEH);
 
-        primaryTabContent.getChildren().add(this.createLabel("Misc"));
-        primaryTabContent.getChildren().add(paneMisc);
+		primaryTabContent.getChildren().add(createLabel("Misc"));
+		primaryTabContent.getChildren().add(paneMisc);
 
-        final FlowPane paneJoeHanson = this.createFlowPane(Sound.BIDDE_QUESTIONING, Sound.BIDDE_IN_TROUBLE, Sound.REVERT, Sound.REWE, Sound.WAS_DU_GLAUBST,
-                Sound.EEHM, Sound.DOMAIN, Sound.SO_NICHT, Sound.WEISST_DUS, Sound.CONVERTER, Sound.COFFEE, Sound.ANNEE_NE);
+		final FlowPane paneJoeHanson = this.createFlowPane(Sound.BIDDE_QUESTIONING, Sound.BIDDE_IN_TROUBLE, Sound.REVERT, Sound.REWE,
+				Sound.EEHM, Sound.DOMAIN, Sound.SO_NICHT, Sound.WEISST_DUS, Sound.CONVERTER, Sound.COFFEE, Sound.ANNEE_NE);
 
-        primaryTabContent.getChildren().add(this.createLabel("Jother"));
-        primaryTabContent.getChildren().add(paneJoeHanson);
-    }
+		primaryTabContent.getChildren().add(createLabel("Jother"));
+		primaryTabContent.getChildren().add(paneJoeHanson);
+	}
 
-    private void createMovieTab() {
+	private void createMovieTab() {
 
-        this.movieTab = new Tab("Filmzitate");
-        this.movieTab.setClosable(false);
-        this.tabPane.getTabs().add(this.movieTab);
+		this.movieTab = new Tab("Filmzitate");
+		this.movieTab.setClosable(false);
+		this.tabPane.getTabs().add(this.movieTab);
 
-        final FlowPane tabContent = new FlowPane();
-        tabContent.setPadding(new Insets(10, 0, 0, 0));
-        tabContent.getStyleClass().add("wood-shifted");
-        this.movieTab.setContent(tabContent);
+		final FlowPane tabContent = new FlowPane();
+		tabContent.setPadding(new Insets(10, 0, 0, 0));
+		tabContent.getStyleClass().add("wood-shifted");
+		this.movieTab.setContent(tabContent);
 
-        final FlowPane panePulpFiction = this.createFlowPane(Sound.WAS_JETZT, Sound.EINMAL_WAS, Sound.ANSTEHT, Sound.KONZEPT, Sound.ZUR_SEITE, Sound.NEIN_MANN);
+		final FlowPane panePulpFiction = this.createFlowPane(Sound.WAS_JETZT, Sound.EINMAL_WAS, Sound.ANSTEHT, Sound.KONZEPT, Sound.ZUR_SEITE, Sound.NEIN_MANN);
 
-        tabContent.getChildren().add(this.createLabel("Pulp Fiction"));
-        tabContent.getChildren().add(panePulpFiction);
+		tabContent.getChildren().add(createLabel("Pulp Fiction"));
+		tabContent.getChildren().add(panePulpFiction);
 
-        final FlowPane pokerPane = this.createFlowPane(Sound.SPIEL_LERNEN);
+		final FlowPane pokerPane = this.createFlowPane(Sound.SPIEL_LERNEN);
 
-        tabContent.getChildren().add(this.createLabel("Poker-Filme"));
-        tabContent.getChildren().add(pokerPane);
-        
-        final FlowPane bangBoomBangPane = this.createFlowPane(Sound.ABSTECHEN, Sound.SCHEISS_LAUNE);
+		tabContent.getChildren().add(createLabel("Poker-Filme"));
+		tabContent.getChildren().add(pokerPane);
 
-        tabContent.getChildren().add(this.createLabel("Bang Boom Bang"));
-        tabContent.getChildren().add(bangBoomBangPane);
-    }
+		final FlowPane bangBoomBangPane = this.createFlowPane(Sound.ABSTECHEN, Sound.SCHEISS_LAUNE);
 
-    private void createClassicsTab() {
+		tabContent.getChildren().add(createLabel("Bang Boom Bang"));
+		tabContent.getChildren().add(bangBoomBangPane);
+	}
 
-        this.newbiesTab = new Tab("Klassiker");
-        this.newbiesTab.setClosable(false);
-        this.tabPane.getTabs().add(this.newbiesTab);
+	private void createClassicsTab() {
 
-        final FlowPane tabContent = new FlowPane();
-        tabContent.setPadding(new Insets(10, 0, 0, 0));
-        tabContent.getStyleClass().add("wood-shifted");
-        this.newbiesTab.setContent(tabContent);
+		this.newbiesTab = new Tab("Klassiker");
+		this.newbiesTab.setClosable(false);
+		this.tabPane.getTabs().add(this.newbiesTab);
 
-        final FlowPane evergreens = this.createFlowPane(Sound.SLAPSTICK);
-        tabContent.getChildren().add(this.createLabel("Sound FX"));
-        tabContent.getChildren().add(evergreens);
-        
-        final FlowPane shrinePanel = this.createFlowPane(Sound.GUDE_LAUNE, Sound.SCHLAEGEREI, Sound.FEIERGEMEINDE);
-        tabContent.getChildren().add(this.createLabel("The Shrine"));
-        tabContent.getChildren().add(shrinePanel);
-        
-        final FlowPane mediamarktPanel = this.createFlowPane(Sound.ZWEIHUNDERT_PULS, Sound.BILDSCHIRM, Sound.SCHEISE);
-        tabContent.getChildren().add(this.createLabel("Mediamarkt"));
-        tabContent.getChildren().add(mediamarktPanel);
-    }
+		final FlowPane tabContent = new FlowPane();
+		tabContent.setPadding(new Insets(10, 0, 0, 0));
+		tabContent.getStyleClass().add("wood-shifted");
+		this.newbiesTab.setContent(tabContent);
 
-    private void addSound(final FlowPane flowPane, final Sound sound) {
-        flowPane.getChildren().add(this.soundButtons.get(sound));
-    }
+		final FlowPane cubageTeamSounds = this.createFlowPane(Sound.SLAPSTICK, Sound.WAS_DU_GLAUBST_JOTHER);
+		tabContent.getChildren().add(createLabel("Cubage Team (2010 - 2012)"));
+		tabContent.getChildren().add(cubageTeamSounds);
 
-    private Label createLabel(final String caption) {
-        final Label label = new Label(caption);
-        label.setTextFill(Color.web("#EEEEEE"));
-        return label;
-    }
+		final FlowPane basteleckeSounds = this.createFlowPane(Sound.BIEKER, Sound.BALLAD);
+		tabContent.getChildren().add(createLabel("Bastelecke Team (2012 - present)"));
+		tabContent.getChildren().add(basteleckeSounds);
 
-    private FlowPane createFlowPane(final Sound... sounds) {
-        final FlowPane pane = this.createFlowPane(true);
+		final FlowPane shrinePanel = this.createFlowPane(Sound.GUDE_LAUNE, Sound.SCHLAEGEREI, Sound.FEIERGEMEINDE);
+		tabContent.getChildren().add(createLabel("The Shrine"));
+		tabContent.getChildren().add(shrinePanel);
 
-        for (final Sound sound : sounds) {
-            this.addSound(pane, sound);
-        }
-        return pane;
-    }
+		final FlowPane mediamarktPanel = this.createFlowPane(Sound.ZWEIHUNDERT_PULS, Sound.BILDSCHIRM, Sound.SCHEISE);
+		tabContent.getChildren().add(createLabel("Mediamarkt"));
+		tabContent.getChildren().add(mediamarktPanel);
+	}
 
-    private FlowPane createFlowPane(final boolean border) {
-        final FlowPane pane = new FlowPane();
-        pane.setPadding(new Insets(5, 5, 5, 5));
-        pane.setVgap(5);
-        pane.setHgap(5);
-        pane.setMinWidth(700);
-        if (border) {
-            pane.getStyleClass().add("button-group");
-        }
-        return pane;
-    }
+	private void addSound(final FlowPane flowPane, final Sound sound) {
+		flowPane.getChildren().add(this.soundButtons.get(sound));
+	}
+
+	private Label createLabel(final String caption) {
+		final Label label = new Label(caption);
+		label.setTextFill(Color.web("#EEEEEE"));
+		return label;
+	}
+
+	private FlowPane createFlowPane(final Sound... sounds) {
+		final FlowPane pane = this.createFlowPane(true);
+
+		for (final Sound sound : sounds) {
+			addSound(pane, sound);
+		}
+		return pane;
+	}
+
+	private FlowPane createFlowPane(final boolean border) {
+		final FlowPane pane = new FlowPane();
+		pane.setPadding(new Insets(5, 5, 5, 5));
+		pane.setVgap(5);
+		pane.setHgap(5);
+		pane.setMinWidth(700);
+		if (border) {
+			pane.getStyleClass().add("button-group");
+		}
+		return pane;
+	}
 }
--- src/main/java/panelfx/Sound.java
+++ src/main/java/panelfx/Sound.java
@@ -2,47 +2,83 @@ package panelfx;
 
 public enum Sound {
 
-	AAAAAAH("Aaaaaaah", "stoehner_fmr"), ALARM("Alarm", "alarm"), ALTOBELLI(
-			"Altobelli", "altobelli"), ANNEE_NE("Annee-ne", "aneene"), ANSTEHT(
-			"Ich sag dir", "ansteht"), ASSERT("Essort", "essort"), BAEM("Bäm",
-			"baehm"), BALLAD("Ballad", "ballad"), BATTLE1("Battle 1",
-			"eurobattle1"), BATTLE3("Battle 3", "eurobattle3"), BIDDE_IN_TROUBLE(
-			"bidde?", "bidde_veraengstigt"), BIDDE_QUESTIONING("BIDDE?",
-			"bidde_fragend"), BIEKER("Bieker", "bieker"), BULLSHIT("Bullshit",
-			"bullshit"), COFFEE("Kaffee", "kaffee"), CONVERTER("Konverter",
-			"konverter"), COOL("Cool", "cool"), CUBAGE("Cubage", "qbitch"), DANKOEOE(
-			"Danköö", "dankoe"), DELETE("Delete", "delate"), DOMAIN("Domäne",
-			"domaene"), DOORBELL("Klingel 1. OG", "klingel"), EEHM("Eehm",
-			"eehm"), EINMAL_WAS("1 mal was", "was"), ERBAERMLICH("Erbärmlich",
-			"erbaermlich"), FAIL("Fail", "gesch_reiner"), FAIL_SATANIC(
-			"Fail Satanic", "gescheitert_satan"), FALSCH("Falsch", "falsch"), FOG(
-			"The Fog", "fog"), FRANK_MUELLER("Frank Müller", "fmueller"), GANZ_KLAR(
-			"Ganz klar", "ganzklar"), HILFE("Hilfe", "hilfe"), HORHOR(
-			"HorHorHor", "kevin"), HUST("Hust", "hust"), JEOPARDY("Jeopardy",
-			"jeopardy"), KHAN("Khan", "khan"), KNARZ("Knarz", "knarz_equalized"), KONZEPT(
-			"Konzept", "konzept"), KOTZEN("Kotzen", "kotzen"), LAUGHING_DIYER(
-			"Höhöhö", "ddr"), LAUSCHANGRIFF("Listener", "lauschangriff"), LOKAL(
-			"Lokal", "lokal"), NEIN_FRALLER("Neeein", "nein_fmr"), NEIN_MANN(
-			"Nein Mann", "neinmann"), NEIN_ULTZ("Neein", "nein"), NJAEH(
-			"Njääh", "guidolf_njaeh"), OEOEOEEY("Öööey", "oey"), PUSH_IT(
-			"Push it", "pushit"), QUIET("Quiet!", "quiet"), REMOTE("Remote",
-			"remoot"), REVERT("Reewert", "reewert"), REWE("Rewe", "rewe"), SCHAFSKAESE(
-			"Schafskäse", "schafskaese"), SLAPSTICK("Slapstick", "hui"), SNAPHOT(
-			"Snapshoot", "snapshoot"), SO_NICHT("So nicht!", "sonicht"), SORRY(
-			"Sorry", "sorry"), SPIEL_LERNEN("Spiel lernen", "erst_noch_lernen"), STANDUP(
-			"Stand up", "standup"), STEELE2("Steele 2", "steele2"), TACH(
-			"Tach", "tachzusammen"), WAS_DENN("Was denn", "wasdenn"), WAS_DU_GLAUBST(
-			"Das ist das", "dasist"), WAS_JETZT("Was jetzt?", "wasjetzt"), WEISST_DUS(
-			"Weißt Du's?", "weisstdus"), WIE_GEIL("Wie geil", "geil"), ZUR_LIEFERUNG(
-			"Zur Lieferung", "lieferung"), ZUR_SEITE("Zur Seite", "butch"), ABSTECHEN(
-			"Stech ihn ab", "abstechen"), SCHEISS_LAUNE("Scheiss Laune",
-			"laune"),
-			GUDE_LAUNE("Gude Laune", "gude_laune"),
-			SCHLAEGEREI("Schlaegerei", "schlaegerei"),
-			FEIERGEMEINDE("Feiergemeinde", "feiergemeinde"),
-			ZWEIHUNDERT_PULS("200 Puls", "200puls"),
-			BILDSCHIRM("Bildschirm", "bildschirm"),
-			SCHEISE("Scheise", "scheise");
+	AAAAAAH("Aaaaaaah", "stoehner_fmr"), //
+	ALARM("Alarm", "alarm"), //
+	ALTOBELLI("Altobelli", "altobelli"), //
+	ANNEE_NE("Annee-ne", "aneene"), //
+	ANSTEHT("Ich sag dir", "ansteht"), //
+	ASSERT("Essort", "essort"), //
+	BAEM("Bäm", "baehm"), //
+	BALLAD("Ballad", "ballad"), //
+	BATTLE1("Battle 1", "eurobattle1"), //
+	BATTLE3("Battle 3", "eurobattle3"), //
+	BIDDE_IN_TROUBLE("bidde?", "bidde_veraengstigt"), //
+	BIDDE_QUESTIONING("BIDDE?", "bidde_fragend"), //
+	BIEKER("Bieker", "bieker"), //
+	BULLSHIT("Bullshit", "bullshit"), //
+	COFFEE("Kaffee", "kaffee"), //
+	CONVERTER("Konverter", "konverter"), //
+	COOL("Cool", "cool"), //
+	CUBAGE("Cubage", "qbitch"), //
+	DANKOEOE("Danköö", "dankoe"), //
+	DELETE("Delete", "delate"), //
+	DOMAIN("Domäne", "domaene"), //
+	DOORBELL("Klingel 1. OG", "klingel"), //
+	EEHM("Eehm", "eehm"), //
+	EINMAL_WAS("1 mal was", "was"), //
+	ERBAERMLICH("Erbärmlich", "erbaermlich"), //
+	FAIL("Fail", "gesch_reiner"), //
+	FAIL_SATANIC("Fail Satanic", "gescheitert_satan"), //
+	FALSCH("Falsch", "falsch"), //
+	FOG("The Fog", "fog"), //
+	FRANK_MUELLER("Frank Müller", "fmueller"), //
+	GANZ_KLAR("Ganz klar", "ganzklar"), //
+	HILFE("Hilfe", "hilfe"), //
+	HORHOR("HorHorHor", "kevin"), //
+	HUST("Hust", "hust"), //
+	JEOPARDY("Jeopardy", "jeopardy"), //
+	KHAN("Khan", "khan"), //
+	KNARZ("Knarz", "knarz_equalized"), //
+	KONZEPT("Konzept", "konzept"), //
+	KOTZEN("Kotzen", "kotzen"), //
+	LAUGHING_DIYER("Höhöhö", "ddr"), //
+	LAUSCHANGRIFF("Listener", "lauschangriff"), //
+	LOKAL("Lokal", "lokal"), //
+	NEIN_FRALLER("Neeein", "nein_fmr"), //
+	NEIN_MANN("Nein Mann", "neinmann"), //
+	NEIN_ULTZ("Neein", "nein"), //
+	NJAEH("Njääh", "guidolf_njaeh"), //
+	OEOEOEEY("Öööey", "oey"), //
+	PUSH_IT("Push it", "pushit"), //
+	QUIET("Quiet!", "quiet"), //
+	REMOTE("Remote", "remoot"), //
+	REVERT("Reewert", "reewert"), //
+	REWE("Rewe", "rewe"), //
+	SCHAFSKAESE("Schafskäse", "schafskaese"), //
+	SLAPSTICK("Slapstick", "hui"), //
+	SNAPHOT("Snapshoot", "snapshoot"), //
+	SO_NICHT("So nicht!", "sonicht"), //
+	SORRY("Sorry", "sorry"), //
+	SPIEL_LERNEN("Spiel lernen", "erst_noch_lernen"), //
+	STANDUP("Stand up", "standup"), //
+	STEELE2("Steele 2", "steele2"), //
+	TACH("Tach", "tachzusammen"), //
+	WAS_DENN("Was denn", "wasdenn"), // //
+	WAS_DU_GLAUBST_JOTHER("Das ist das", "dasist"), //
+	WAS_DU_GLAUBST_MAROHDE("Was Du glaubst", "was_du_glaubst"), //
+	WAS_JETZT("Was jetzt?", "wasjetzt"), //
+	WEISST_DUS("Weißt Du's?", "weisstdus"), //
+	WIE_GEIL("Wie geil", "geil"), //
+	ZUR_LIEFERUNG("Zur Lieferung", "lieferung"), //
+	ZUR_SEITE("Zur Seite", "butch"), //
+	ABSTECHEN("Stech ihn ab", "abstechen"), //
+	SCHEISS_LAUNE("Scheiss Laune", "laune"), //
+	GUDE_LAUNE("Gude Laune", "gude_laune"), //
+	SCHLAEGEREI("Schlaegerei", "schlaegerei"), //
+	FEIERGEMEINDE("Feiergemeinde", "feiergemeinde"), //
+	ZWEIHUNDERT_PULS("200 Puls", "200puls"), //
+	BILDSCHIRM("Bildschirm", "bildschirm"), //
+	SCHEISE("Scheise", "scheise");
 
 	private String label;
 	private String filename;
