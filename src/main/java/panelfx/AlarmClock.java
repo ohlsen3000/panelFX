fb7112e5cf31e8455ea3df021f2893955e24aa72
Johannes Günther <jgr@timocom.com>
2013-04-25 10:47:59 +0200

    fix security issue in AlarmClock

--- src/main/java/panelfx/AlarmClock.java
+++ src/main/java/panelfx/AlarmClock.java
@@ -1,5 +1,7 @@
 package panelfx;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Timer;
@@ -7,58 +9,62 @@ import java.util.TimerTask;
 
 public class AlarmClock implements Runnable {
 
-	private final String user;
+    private final String user;
 
-	private final int hour;
-	private final int minute;
+    private final int hour;
+    private final int minute;
 
-	private final PlayingSounds playingSounds;
+    private final PlayingSounds playingSounds;
 
-	public AlarmClock(final PlayingSounds playingSounds) {
-		super();
-		this.playingSounds = playingSounds;
-		this.user = System.getProperty("user.name");
-		this.hour = 10;
-		this.minute = 0;
-	}
+    public AlarmClock(final PlayingSounds playingSounds) {
+        super();
+        this.playingSounds = playingSounds;
+        this.user = System.getProperty("user.name");
+        this.hour = 10;
+        this.minute = 0;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		if ("ugz".equalsIgnoreCase(this.user)){
+        try {
+            if ("CL-CZC0186XJH".equalsIgnoreCase(InetAddress.getLocalHost().getHostName())) {
 
-			final Calendar now = new GregorianCalendar();
-			final int hourNow = now.get(Calendar.HOUR_OF_DAY);
-			final int minuteNow = now.get(Calendar.MINUTE);
+                final Calendar now = new GregorianCalendar();
+                final int hourNow = now.get(Calendar.HOUR_OF_DAY);
+                final int minuteNow = now.get(Calendar.MINUTE);
 
-			final Calendar firstExecutionDate = new GregorianCalendar();
-			firstExecutionDate.set(Calendar.HOUR_OF_DAY, this.hour);
-			firstExecutionDate.set(Calendar.MINUTE, this.minute);
-			firstExecutionDate.set(Calendar.SECOND, 0);
-			firstExecutionDate.set(Calendar.MILLISECOND, 0);
-			if (this.hour<hourNow || this.hour==hourNow && this.minute<minuteNow) {
-				//Do not execute today, first execution will be tomorrow.
-				firstExecutionDate.roll(Calendar.DAY_OF_MONTH, true);
-			}
+                final Calendar firstExecutionDate = new GregorianCalendar();
+                firstExecutionDate.set(Calendar.HOUR_OF_DAY, this.hour);
+                firstExecutionDate.set(Calendar.MINUTE, this.minute);
+                firstExecutionDate.set(Calendar.SECOND, 0);
+                firstExecutionDate.set(Calendar.MILLISECOND, 0);
+                if ((this.hour < hourNow) || ((this.hour == hourNow) && (this.minute < minuteNow))) {
+                    //Do not execute today, first execution will be tomorrow.
+                    firstExecutionDate.roll(Calendar.DAY_OF_MONTH, true);
+                }
 
-			final long oneDay = 1000L * 60L * 60L * 24L;
+                final long oneDay = 1000L * 60L * 60L * 24L;
 
-			System.out.println("First execution " + firstExecutionDate.toString());
-			final Timer timer = new Timer();
-			timer.schedule(new StandupSoundTimer(), firstExecutionDate.getTime(), oneDay);
+                System.out.println("First execution " + firstExecutionDate.toString());
+                final Timer timer = new Timer();
+                timer.schedule(new StandupSoundTimer(), firstExecutionDate.getTime(), oneDay);
 
-		}
+            }
+        } catch (final UnknownHostException e) {
+            System.out.println("no alarm clock: " + e.getMessage());
+        }
 
-	}
+    }
 
-	protected class StandupSoundTimer extends TimerTask {
+    protected class StandupSoundTimer extends TimerTask {
 
-		@Override
-		public void run() {
-			final String url = PanelFX.class.getResource(Sound.STANDUP.getFilename() + ".mp3").toString();
-			final SoundThread sound = new SoundThread(url, AlarmClock.this.playingSounds);
-			sound.run();
-		}
+        @Override
+        public void run() {
+            final String url = PanelFX.class.getResource(Sound.STANDUP.getFilename() + ".mp3").toString();
+            final SoundThread sound = new SoundThread(url, AlarmClock.this.playingSounds);
+            sound.run();
+        }
 
-	}
+    }
 }
